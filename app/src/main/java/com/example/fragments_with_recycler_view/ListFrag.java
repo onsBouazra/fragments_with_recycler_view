b58b2472dbbe59eb6fcec560845767b81a577861
ons bouwazra <ons.bouwazra9@gmail.com>
2020-06-10 17:18:06 +0200

    second commit

---
+++ app/src/main/java/com/example/fragments_with_recycler_view/ListFrag.java
@@ -0,0 +1,37 @@
+package com.example.fragments_with_recycler_view;
+
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListFrag extends ListFragment {
+
+
+    public ListFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+}
---
+++ app/src/main/java/com/example/fragments_with_recycler_view/NewContactFraf.java
@@ -0,0 +1,31 @@
+package com.example.fragments_with_recycler_view;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NewContactFraf extends Fragment {
+
+
+    public NewContactFraf() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_contact_fraf, container, false);
+    }
+
+}
